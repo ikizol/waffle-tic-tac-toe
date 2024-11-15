ccc8ea6afa3cc11cb9a8ab216f874ee54b0e0f35
ikizol <93149164+ikizol@users.noreply.github.com>
2024-11-14 22:38:45 -0500

    Update script.js

--- script.js
+++ script.js
@@ -63,3 +63,29 @@ function isDraw() {
 		return cell.classList.contains(PLAYER_X_CLASS) || cell.classList.contains(PLAYER_O_CLASS)
 	})
 }
+
+function placeMark(cell, currentClass) {
+	cell.classList.add(currentClass)
+}
+
+function swapTurns() {
+	isPlayer_O_Turn = !isPlayer_O_Turn
+}
+
+function setBoardHoverClass() {
+	boardElement.classList.remove(PLAYER_X_CLASS)
+	boardElement.classList.remove(PLAYER_O_CLASS)
+	if (isPlayer_O_Turn) {
+		boardElement.classList.add(PLAYER_O_CLASS)
+	} else {
+		boardElement.classList.add(PLAYER_X_CLASS)
+	}
+}
+
+function checkWin(currentClass) {
+	return WINNING_COMBINATIONS.some(combination => {
+		return combination.every(index => {
+			return cellElements[index].classList.contains(currentClass)
+		})
+	})
+}
